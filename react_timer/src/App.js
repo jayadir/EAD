36827d41c22c973d9ff4ec68821c86d696c553cb
jayadir <jayadir102005@gmail.com>
2024-11-04 10:20:31 +0530

    added timer

---
+++ react_timer/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  const reset = () => {
+    setSeconds(0);
+    setIsActive(false);
+  };
+
+  return (
+    <div className="timer">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
